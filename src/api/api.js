c686b48094e320aed390e01713a48ed2a60434de
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-05-07 10:11:23 -0600

    install: dotenv, api boilerplate in place.

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const client = axios.create({
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://api.bundleofsage.com"
+      : "http://localhost:3061",
+  withCredentials: true
+});
+
+export default {
+  auth: {
+    checkStatus: () => client.get("/auth/status"),
+    signUp: userInfo => client.post("/auth/sign-up", { userInfo }),
+    login: userInfo => client.post("/auth/login", { userInfo }),
+    logout: () => client.get("/auth/logout")
+  },
+  membership: {}
+};
