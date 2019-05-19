0fd8a02057484ce1ace802fca3b3a0e7e509b518
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-05-19 14:22:24 -0600

    feat: sidebar click off listener.

--- src/components/Layout/MobileBar/MobileBar.js
+++ src/components/Layout/MobileBar/MobileBar.js
@@ -4,6 +4,16 @@ import Sidebar from "./Sidebar/Sidebar";
 
 export default class MobileBar extends Component {
   state = { visible: false };
+  mobileBarRef = React.createRef();
+  componentDidMount() {
+    this.mobileBarRef.current.addEventListener("click", e => {
+      //If click occurs on shadow, toggle sidebar closed
+      const { className } = e.target;
+      if (className.includes("shadow")) {
+        this.setState({ visible: false });
+      }
+    });
+  }
   toggleSidebar = () => {
     this.setState(state => {
       return { visible: !state.visible };
@@ -12,7 +22,7 @@ export default class MobileBar extends Component {
   render() {
     const { visible } = this.state;
     return (
-      <div className={classes.outerContainer}>
+      <div className={classes.outerContainer} ref={this.mobileBarRef}>
         <div className={classes.container}>
           <h1>Bundle of Sage</h1>
           <i className="fas fa-equals" onClick={this.toggleSidebar} />
