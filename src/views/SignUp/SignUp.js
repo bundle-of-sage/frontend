760e1b3c45ba649ab7a8971a8cced41dc75b1a13
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-05-19 12:19:56 -0600

    feat: feature detail UI for sign up.

--- src/views/SignUp/SignUp.js
+++ src/views/SignUp/SignUp.js
@@ -12,7 +12,25 @@ export default class SignUp extends Component {
       <div className={classes.container}>
         <div className={classes.paymentContainer}>
           <h3>Join Bundle of Sage</h3>
-          <h6>$50 USD - MEMBERSHIP ACCESS</h6>
+
+          <div className={classes.featureContainer}>
+            <div>
+              <h6>$50 USD - MEMBERSHIP ACCESS</h6>
+            </div>
+            <div style={{ alignItems: "flex-end" }}>
+              <p>
+                <i className="far fa-calendar" /> No Subscription
+              </p>
+              <p>
+                <i className="fas fa-columns" />
+                New Content Added
+              </p>
+              <p>
+                <i className="fas fa-box-open" />
+                All in One Place
+              </p>
+            </div>
+          </div>
           <StripeProvider apiKey={REACT_APP_STRIPE_KEY}>
             <Elements fonts={fonts}>
               <InjectedSubForm {...this.props} />
