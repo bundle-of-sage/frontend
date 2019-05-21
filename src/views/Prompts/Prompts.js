d85894235916d988198ea52aacf878621497f537
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-05-20 18:26:33 -0600

    feat: category UI.

--- src/views/Prompts/Prompts.js
+++ src/views/Prompts/Prompts.js
@@ -7,6 +7,15 @@ export default class Prompts extends Component {
     return (
       <div className={classes.container}>
         <h1>Prompts</h1>
+        <div className={classes.category} style={{ background: "#97422b" }}>
+          <h1>Fun</h1>
+        </div>
+        <div className={classes.category} style={{ background: "#c17c5b" }}>
+          <h1>Romantic</h1>
+        </div>
+        <div className={classes.category} style={{ background: "#fcd0b3" }}>
+          <h1>Intimate</h1>
+        </div>
       </div>
     );
   }
