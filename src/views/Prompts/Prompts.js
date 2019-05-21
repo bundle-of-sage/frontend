bfecf369565d97049a4b50977bfe08874935b883
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-05-20 18:17:25 -0600

    feat: card UI.

--- src/views/Prompts/Prompts.js
+++ src/views/Prompts/Prompts.js
@@ -11,6 +11,14 @@ export default class Prompts extends Component {
     return (
       <div className={classes.container}>
         <h1>Prompts</h1>
+        <div className={classes.promptCard}>
+          <div className={classes.promptHeader}>Romantic</div>
+          <p className={classes.promptText}>
+            Guy: lay down and raise your legs in the air. Girl: climb on so your
+            hips are resting on his feet and grab his hands for support. Fly her
+            around like an airplane, sound effects are encouraged.
+          </p>
+        </div>
       </div>
     );
   }
