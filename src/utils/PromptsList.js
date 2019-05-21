3dac977c7a3f83883c0eec8654d3528fdf3f9e74
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-05-20 18:08:25 -0600

    feat: intial prompt list data.

---
+++ src/utils/PromptsList.js
@@ -0,0 +1,430 @@
+const funPrompts = [
+  {
+    prompt:
+      "Run towards each other and embrace like a rom-com airport scene. Pick her up, kiss, whatever you feel like!",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Run up behind her and grab her like you’re kidnapping her. Kick wildy and try to escape.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Throw her over your shoulder like a potato sack. Sprint for the finish line.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt: "Lift her up and spin her around.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt: "Jump up and down like you just won a million dollars.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Jump high in the air with your knees and elbows bent. Run like you’re mario and luigi in an old school nintendo game.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Hold hands and spin each other. Try to kiss a different body part after each spin. Try not to fall over!",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Stand behind your girl, wrap your arms around her, and swing her around like dead weight.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Machine gun kisses: kiss each other as many times as you can in 10 seconds.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Play a game of tag right now. You must touch his/her butt to win the game of tag.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt: "Dance like you’re on dancing with the stars.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Act like you’re 12 year olds chasing and flirting with each other.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt: "Change your expressions every three seconds like models.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt: "Make your partner laugh using nothing but your eyebrows.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt: "Get in close and give me your best monkey impersonation",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt: "Yell your favorite swear words at the camera.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Try talking about each other in ridiculous accents with a straight face.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Have one of them imitate being the other person, and to do the most *insert name* things they can think of.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Guy: lay down and raise your legs in the air. Girl: climb on so your hips are resting on his feet and grab his hands for support. Fly her around like an airplane, sound effects are encouraged.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Stand side by side an arm-width apart facing the camera. Now walk towards me as seriously hipster as possible while looking into the camera.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Stand behind her, wrap your arms around. Whisper in your sexiest voice, types of cereal.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt: "Pull your partner towards you and then apart and kiss in between.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt: "Whisper your least favorite chores in your sexiest voice.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "List your favorite breakfast foods in a british accent in her ear.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Hold each other in a classic cheesy prom pose. Now lean over and sniff your girl’s ear.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Get comfy on a rock and sit on his lap. You’re partner’s arms are freeeeezing! Warm them up.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Have a staring contest and whoever blinks first has to cook dinner. And tell them, “Don’t worry. I have your phone number. I”ll be calling later to make sure!”",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Squish your cheeks together all cheesy. Stick your tongues out and try to lick each other.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Guy stand facing me, girl come up from behind and wrap your arms around him or just one arm. Nuzzle into his ear and give it a love bite.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt: "Nuzzle into your partner’s face (like a cat) and purr",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Get in close (sit, snuggle, etc). What’s the weirdest thing you love about each other.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Hug his back and close your eyes. Try to hear his heartbeat, can’t hear it? Squeeze him really tight!",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Her: wrap your arms around your partner in a great big bear hug and look over his shoulder at me. Tickle the little hairs on the back of his neck.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Put your arm around her shoulders like a rockstar. Whisper in her ear the weirdest thing you love about her.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Sit back to back and rest into each other. List the clothes that your partner wears that you hate.",
+    hint: "",
+    starred: false,
+    attribute: null
+  }
+];
+
+const romanticPrompts = [
+  {
+    prompt:
+      "Hug each other like you won’t be seeing one another for 6 months. Tell your partner about the time where you needed them most.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Get close for a kiss, but no kissing until I say so. Kiss your favorite part of your partners face.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Slow dance together, foreheads touching. Pick a song to hum, your partner has to try to guess it.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Wrap her up and kiss her forehead. Who said “i love you” first, what happened next?",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Move her hair away from her face, grab her cheek and pull her in. With your nose draw a shape on her cheek, try to guess what it is.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Face each other, rub up and down her arm, now kiss her head lovingly. Tell the story of how you first met, but each sentence must start with the next letter of the alphabet.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Face each other, and hold hands together under your chin. With your faces as close as possible, try to express just how much you love each other using only your eyes.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Her: rest your head on his chest and look at me. Him: wrap her up and look over her head into the distance. Her: stare right into my lens and think about the most loving thing your partner has ever done for you.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Stand in front of your man and reach your arm up around his neck. Look up and smile at him, like it’s the first time you’ve seen each other in weeks.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Guy take the lead and help your girl along. Look back at your girl and smile like it’s the first time you ever saw her.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Stand side by side holding hands close together, girl hug his harm and rest on his shoulder. Stare deep into each other’s eyes. When I count to 3, try and guess the color the other is thinking.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Lay in his lap. Stare up into his eyes and hum a song and he has to guess what song.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "While sitting down, tie yourselves into a human pretzel. Act like it’s freezing outside and you must be as close as possible to keep each other warm.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Sit slightly behind and wrap your arms around your guy. Tell him the first thing you thought when you first met him.",
+    hint: "",
+    starred: false,
+    attribute: null
+  }
+];
+
+const intimatePrompts = [
+  {
+    prompt: "Hold each other close and breath each other in.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Close your eyes and rest your noses together. Now grab each other’s faces with the hand closest to me. Eyes still closed, think about how far you’ve come as a couple and all that you’ve experienced together.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Lay your head on his chest and close your eyes, both of you. Listen to his heartbeat.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Foreheads together with your eyes closed, use your fingers to trace along their jaw line.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Wrap up your hands and pull his in for a tender kiss. Give his hand a love bite.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Stand nose to nose, hands rubbing each others arms. Think of a time where you were just bursting with love for your partner, now whisper that moment in their ear.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "With him sitting behind you, embrace each other, then rest your cheek in his arms. Close your eyes and think about the first time you realized you loved each other, now share with your partner.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Sit in between his legs, and rest back into him. Take turns telling each other the top 5 things you’re most grateful for. Wrap arms.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Sit side by side leaning into each other. Tell him/her about the moment you decided that he/she was the one.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Sit face to face, get in close, legs over each other. Take turns telling one another three things you always promise to do for the other.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Lay down side by side and cuddle into each other's faces. Pretend it’s sunday morning and you’re snuggling in bed.",
+    hint: "",
+    starred: false,
+    attribute: null
+  },
+  {
+    prompt:
+      "Lay together on your tummies. Before the shoot as them to write a letter to each other, favorite memories, what they love most, etc.",
+    hint: "",
+    starred: false,
+    attribute: null
+  }
+];
+
+module.exports = {
+  funPrompts,
+  romanticPrompts,
+  intimatePrompts
+};
--- src/views/Prompts/Prompts.js
+++ src/views/Prompts/Prompts.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 import classes from "./Prompts.module.scss";
+import {
+  funPrompts,
+  romanticPrompts,
+  intimatePrompts
+} from "../../utils/PromptsList";
 
 export default class Prompts extends Component {
   render() {
-    return <div className={classes.container}>Prompts</div>;
+    return (
+      <div className={classes.container}>
+        <h1>Prompts</h1>
+      </div>
+    );
   }
 }
